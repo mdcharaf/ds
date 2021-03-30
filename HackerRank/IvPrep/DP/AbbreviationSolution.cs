f32c1bbbd2800e1fb6392992a99b501b90f61967
Muhammad Charaf <muhammad.charaf@gmail.com>
2021-03-30 03:39:39 +0200

    WIP

--- HackerRank/IvPrep/DP/AbbreviationSolution.cs
+++ HackerRank/IvPrep/DP/AbbreviationSolution.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace HackerRank.IvPrep.DP
@@ -15,44 +16,44 @@ namespace HackerRank.IvPrep.DP
 
         private static bool CanConstruct(string source, string target, IDictionary<string, bool> memo)
         {
-            if (memo.ContainsKey(source))
-                return memo[source];
-            
             if (source.Length < target.Length)
                 return false;
 
-            if (source.Length == target.Length)
-                return source == target;
+            if (memo.ContainsKey(source))
+                return memo[source];
 
             for (int i = 0; i < source.Length; i++)
             {
                 if (char.IsUpper(source[i]))
                 {
+                    // if Exists an upper character at the end of the string || sourceChar != targetChar
                     if (i >= target.Length || source[i] != target[i])
+                    {
+                        memo.Add(source, false);
                         return false;
+                    }
                 }
                 else
                 {
                     var removeSourceChar = source.Remove(i, 1);
                     var replaceSourceChar = removeSourceChar.Insert(i, char.ToUpper(source[i]).ToString());
-
-                    var removeSourceCharResult = CanConstruct(removeSourceChar, target, memo);
-                    var replaceSourceCharResult = CanConstruct(replaceSourceChar, target, memo);
                     
-                    memo.Add(removeSourceChar, removeSourceCharResult);
-                    memo.Add(replaceSourceChar, replaceSourceCharResult);
+                    var result = CanConstruct(removeSourceChar, target, memo) ||
+                                 CanConstruct(replaceSourceChar, target, memo);
                     
-                    return removeSourceCharResult || replaceSourceCharResult;
+                    memo.Add(source, result);
+                    return result;
                 }
             }
 
+            memo.Add(source, true);
             return true;
         }
 
         public static void RunMain()
         {
             int q = Convert.ToInt32(Console.ReadLine());
-            var str = new StringBuilder();
+            var stringBuilder = new StringBuilder();
 
             for (int qItr = 0; qItr < q; qItr++)
             {
@@ -62,11 +63,12 @@ namespace HackerRank.IvPrep.DP
 
                 string result = Abbreviation(a, b);
 
-                Console.WriteLine(result);
+                // Console.WriteLine(result);
+                stringBuilder.AppendLine(result);
             }
 
-            // Console.Clear();
-            // Console.WriteLine(str.ToString());
+            Console.Clear();
+            Console.WriteLine(stringBuilder.ToString());
         }
 
         static void Main2(string[] args)
